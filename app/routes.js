3acc0947f84f4a211a125f3404f29d7292c413aa
JiggsNHS <julian.williamson1@nhs.net>
2026-09-07 13:54:41 +0000

    Add ineligible page and routing

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,26 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/magical-powers-answer', function (req, res) {
+  const data = req.session.data
+  const hasMagicalPowers = data.hasMagicalPowers
+
+  if (hasMagicalPowers === "Yes") {
+
+    res.redirect('/details')
+
+  } else if (hasMagicalPowers === "No") {
+
+    res.redirect('/ineligible-page')
+
+  
+
+  } else {
+
+    // No answer selected, return to question
+    res.redirect('/magical-powers')
+
+  }
+})
 
 module.exports = router
